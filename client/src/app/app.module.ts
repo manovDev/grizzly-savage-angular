d459d30f170c106c3c6490fa4d6ee27a29b136e9
daleksiev <ivoivchov@gmail.com>
2021-08-18 19:55:48 +0300

    Add main layout component
    #4

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutusComponent
+    AboutusComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
