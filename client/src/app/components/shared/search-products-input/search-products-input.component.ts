c6f4bb8c8910ed4e48b0bd483e042747432252db
daleksiev <ivoivchov@gmail.com>
2021-08-18 21:33:38 +0300

    add search product input component content and style
    #4

---
+++ client/src/app/components/shared/search-products-input/search-products-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-products-input',
+  templateUrl: './search-products-input.component.html',
+  styleUrls: ['./search-products-input.component.scss']
+})
+export class SearchProductsInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
