0df8ca77e41dfe47976e13b0a516e7fbf65f5c6a
daleksiev <ivoivchov@gmail.com>
2021-08-18 21:37:21 +0300

    add cartlink component content and style
    #4

---
+++ client/src/app/components/shared/cartlink/cartlink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cartlink',
+  templateUrl: './cartlink.component.html',
+  styleUrls: ['./cartlink.component.scss']
+})
+export class CartlinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
