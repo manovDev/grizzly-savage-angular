035d6840c28ae89a8d43612af57646b376b97bef
Ivo Manov <ivoivchov@gmail.com>
2021-08-18 22:01:42 +0300

    fix server errors
    #10

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -2,8 +2,8 @@ var admin = require("firebase-admin");
 
 // var serviceAccount = require("./grizzly-savage-firebase-adminsdk-qowzk-907964423a.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
 
 module.exports = admin;
