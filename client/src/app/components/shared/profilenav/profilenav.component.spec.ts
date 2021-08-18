853a4cfeae813bbf895ec7de5b333514a98fc18b
daleksiev <ivoivchov@gmail.com>
2021-08-18 21:34:57 +0300

    add profilenav component content and style
    #4

---
+++ client/src/app/components/shared/profilenav/profilenav.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilenavComponent } from './profilenav.component';
+
+describe('ProfilenavComponent', () => {
+  let component: ProfilenavComponent;
+  let fixture: ComponentFixture<ProfilenavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilenavComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfilenavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
