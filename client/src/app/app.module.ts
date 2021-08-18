ca9101f482f82727e0d294dc7e9ec27ca3bbeff3
daleksiev <ivoivchov@gmail.com>
2021-08-18 20:04:57 +0300

    Add header component
    #4

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { MainLayoutComponent } from './components/layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutusComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
