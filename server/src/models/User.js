89c705d6a02a9989fa376657a3cdd079e62fe4d9
Ivo Manov <ivoivchov@gmail.com>
2021-08-18 22:00:21 +0300

    create User model
    #9

---
+++ server/src/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    uid: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    profileImage: {
+        type: String,
+        required: true
+    },
+    orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order',
+        default: []
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const model = mongoose.model('User', UserSchema);
+module.exports = model;
