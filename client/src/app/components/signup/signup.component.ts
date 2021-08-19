3f9b410e24932113e6cb2e48d502556c6120ddfc
daleksiev <ivoivchov@gmail.com>
2021-08-19 21:34:45 +0300

    Add Signup functionality
    #15

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SignupService } from '../../services/signup/signup.service';
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    constructor(private service: SignupService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    signup(event: Event,firstName: string, lastName: string, email: string, password: string) {
+        event.preventDefault();
+
+        const body: any = {
+            firstName,
+            lastName,
+            email,
+            password,
+            profileImage: ''
+        };
+        
+        this.service.signup(body).subscribe(data => console.log(data));
+    }
 
 }
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema({
     },
     profileImage: {
         type: String,
-        required: true
+        //required: true
     },
     orders: [{
         type: mongoose.Schema.Types.ObjectId,
