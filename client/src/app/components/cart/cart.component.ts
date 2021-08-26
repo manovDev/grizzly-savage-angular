515f5edef5cd55dfa221a8f46bc8bdb0a84f6043
daleksiev <ivoivchov@gmail.com>
2021-08-26 22:36:32 +0300

    Add checkout button for logged in users and login button for guests
    #21

--- client/src/app/components/cart/cart.component.ts
+++ client/src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart/cart.service';
+import { SigninService } from '../../services/signin/signin.service';
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-cart',
     templateUrl: './cart.component.html',
@@ -9,12 +11,15 @@ export class CartComponent implements OnInit {
     products: any = [];
     totalPrice: number = 0;
     productCounter: any = { units: 1 };
+    loggedIn = false;
     handleCounter: Function = (event: Event) => {
 
     };
 
     constructor(
-        private cartService: CartService
+        private cartService: CartService,
+        private auth: SigninService,
+        private router: Router
     ) {
         this.cartService.cart$.subscribe((data: any) => {
             this.products = data;
@@ -24,6 +29,15 @@ export class CartComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.auth.isLoggedIn().then((isLoggedIn: boolean) => this.loggedIn = isLoggedIn);
+    }
+
+    redirectToLogin() {
+        this.router.navigate(['sign-in']);
+    }
+
+    checkout() {
+        this.router.navigate(['order/shipping']);
     }
 
     increaseCount(productId: any) {
