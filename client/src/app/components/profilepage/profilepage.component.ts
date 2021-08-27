295ac5d74019356d476da1d9c67c7e94cb406aad
daleksiev <ivoivchov@gmail.com>
2021-08-28 02:00:20 +0300

    Add profile data into profile page
    #32

--- client/src/app/components/profilepage/profilepage.component.ts
+++ client/src/app/components/profilepage/profilepage.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-profilepage',
-  templateUrl: './profilepage.component.html',
-  styleUrls: ['./profilepage.component.scss']
+    selector: 'app-profilepage',
+    templateUrl: './profilepage.component.html',
+    styleUrls: ['./profilepage.component.scss']
 })
 export class ProfilepageComponent implements OnInit {
+    user: any = {};
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        const user = localStorage.getItem('user');
+        this.user = user ? JSON.parse(user) : {};
+    }
 
 }
